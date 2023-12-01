f18a8b5563b485b3cef848a05b72dfbcc3f036fd
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-12-01 16:46:49 +0530

    posting data to exercise t4

--- index.js
+++ index.js
@@ -93,10 +93,8 @@ app.get("/api/users", (req, res) => {
 app.post("/api/users/:_id/exercises", async (req, res) => {
   console.log(req.params._id);
   if (!req.params._id) res.json({ status: "Invalid request" });
-  const username = await User.findOne({ _id: req.params._id });
-  console.log(username);
+  const username = await User.findOne({ _id: req.params._id }).username;
 
-  console.log(req.body);
   if (!req.body.description || !req.body.duration) {
     return res.json({ status: "Invalid request" });
   }
