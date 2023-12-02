3677fa2192c1e07b37285fc72a69fa2aab968e2b
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-12-02 11:15:08 +0530

    updating logs along with exercies t6

--- index.js
+++ index.js
@@ -116,11 +116,7 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
         ...user,
         ...exerciseObject,
         log: [...user.log, exerciseObject],
-      };
-
-      console.log("user after update");
-      console.log(user);
-      user
+      }
         .save()
         .then((user) => {
           res
